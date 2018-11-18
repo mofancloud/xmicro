9f4003fc824049de22bc274b0570e7df5b96883d
newtalentxp_ali <52388483@qq.com>
2018-11-18 16:39:52 +0800

    f

--- utils/pagination/paginator.go
+++ utils/pagination/paginator.go
@@ -1,19 +1,19 @@
 package pagination
 
-import(
-	"net/http"
-	"io/ioutil"
+import (
 	"encoding/json"
+	"io/ioutil"
+	"net/http"
 )
 
-func ParsePageQuery(req *http.Request): (*PageQuery, error) {
+func ParsePageQuery(req *http.Request) (*PageQuery, error) {
 	defer req.Body.Close()
-	in, _ := ioutil.ReadAll(r.Body) //获取post的数据
+	in, _ := ioutil.ReadAll(req.Body) //获取post的数据
 
 	var pageQuery PageQuery
 	err := json.Unmarshal(in, &pageQuery)
 	if err != nil {
 		return nil, err
 	}
-	return pageQuery, nil
+	return &pageQuery, nil
 }
